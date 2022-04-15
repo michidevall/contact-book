5edf5afc47b2199d9dc32820e654b4aaa012a65c
michidevall <glitzpixy1@yahoo.com>
2022-04-15 13:53:44 -0600

    third commit

--- contacts/main.py
+++ contacts/main.py
@@ -1,10 +1,15 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+
+from .database import createConnection
 from .views import Window
 
 def main():
     # create the application
     app = QApplication(sys.argv)
+    # connect to the database before creating any window
+    if not createConnection("contacts.sqlite"):
+        sys.exit(1)
     # create the main window
     win = Window()
     win.show()
